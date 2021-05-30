b9a69551ac5032070e0c10673faa58aef9e873ce
Adem Mrizak <adem.mrizak@gmail.com>
2021-05-30 17:51:52 +0200

    Check if exists routes

--- src/api/routes/todolist.route.ts
+++ src/api/routes/todolist.route.ts
@@ -18,4 +18,16 @@ router.route("/")
         return response.sendStatus(400);
     });
 
+router.route("/exists")
+    .get(async (request: Request, response: Response) => {
+        const mail = request.query.mail as string;
+        if (mail === undefined) {
+            return response.sendStatus(400);
+        }
+
+        const result = new ToDoListController().checkIfExists(mail);
+
+        return response.json({exists: result});
+    });
+
 export default router;
--- src/api/routes/user.route.ts
+++ src/api/routes/user.route.ts
@@ -28,4 +28,16 @@ router.route("/")
         return response.sendStatus(400);
     });
 
+router.route("/exists")
+    .get(async (request: Request, response: Response) => {
+        const mail = request.query.mail as string;
+        if (mail === undefined) {
+            return response.sendStatus(400);
+        }
+
+        const user = new UserController().getByMail(mail);
+
+        return response.json({exists: user !== null});
+    });
+
 export default router;
